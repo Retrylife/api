59c6e2453639fb67b98c7f237c3097c31a127174
Evan Pratten <ewpratten@gmail.com>
2019-04-09 07:41:51 -0400

    added tba api endpoint

---
+++ api/auth/tba.php
@@ -0,0 +1,6 @@
+<?php
+    header('Content-Type: application/json');
+
+    // temp
+    echo json_encode(['success' => FALSE, 'key' => 'QPI1VLcQrowB0Oq8G0NdTjk30HpSaJ4fJuO4GV29ATKJkJIS6GNVZ1qnlLg0O6Ql']);
+?>
