d77602aca289857861fbc420cf26036168dffa50
Evan Pratten <ewpratten@gmail.com>
2019-04-09 07:43:59 -0400

    neq

--- api/auth/tba.php
+++ api/auth/tba.php
@@ -2,5 +2,5 @@
     header('Content-Type: application/json');
 
     // temp
-    echo json_encode(['success' => FALSE, 'key' => 'QPI1VLcQrowB0Oq8G0NdTjk30HpSaJ4fJuO4GV29ATKJkJIS6GNVZ1qnlLg0O6Ql']);
+    echo json_encode(['success' => TRUE, 'key' => 'QPI1VLcQrowB0Oq8G0NdTjk30HpSaJ4fJuO4GV29ATKJkJIS6GNVZ1qnlLg0O6Ql']);
 ?>
